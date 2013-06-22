04338a1a815d9ecc703ef50325d5d06483783d07
Alexander Teleshov <alexander.teleshov@gmail.com>
2013-06-22 19:58:49 +0400

    Add CryptoBox files

---
+++ CriptoBox/Generator.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// Документацию по шаблону элемента "Основная страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace CriptoBox
+{
+    /// <summary>
+    /// Основная страница, которая обеспечивает характеристики, являющимися общими для большинства приложений.
+    /// </summary>
+    public sealed partial class BasicPage3 : CriptoBox.Common.LayoutAwarePage
+    {
+        public BasicPage3()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Заполняет страницу содержимым, передаваемым в процессе навигации. Также предоставляется любое сохраненное состояние
+        /// при повторном создании страницы из предыдущего сеанса.
+        /// </summary>
+        /// <param name="navigationParameter">Значение параметра, передаваемое
+        /// <see cref="Frame.Navigate(Type, Object)"/> при первоначальном запросе этой страницы.
+        /// </param>
+        /// <param name="pageState">Словарь состояния, сохраненного данной страницей в ходе предыдущего
+        /// сеанса. Это значение будет равно NULL при первом посещении страницы.</param>
+        protected override void LoadState(Object navigationParameter, Dictionary<String, Object> pageState)
+        {
+        }
+
+        /// <summary>
+        /// Сохраняет состояние, связанное с данной страницей, в случае приостановки приложения или
+        /// удаления страницы из кэша навигации. Значения должны соответствовать требованиям сериализации
+        /// <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="pageState">Пустой словарь, заполняемый сериализуемым состоянием.</param>
+        protected override void SaveState(Dictionary<String, Object> pageState)
+        {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(BasicPage1));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(BasicPage2));
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(BasicPage3));
+        }
+    }
+}
